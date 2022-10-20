bf773f04e6ae8aac49ad1f8d57eb8f92392bcc35
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-10-20 19:16:30 -0300

    add libs cors and express

---
+++ src/shared/http/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import 'express-async-errors'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Olá Dev!' })
+})
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000!')
+})
