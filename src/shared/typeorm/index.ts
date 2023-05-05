965a8b6983153bcfed0d25d4ba9c1a0103ded440
matheus damiao <matheus.damiaoliveira@gmail.com>
2023-05-05 12:30:22 -0300

    Create user entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -3,11 +3,12 @@ import { DataSource } from 'typeorm'
 import { CreateRolesTable1666792375084 } from './migrations/1666792375084-CreateRolesTable'
 import { CreateUsersTable1683038820881 } from './migrations/1683038820881-CreateUsersTable'
 import { AddRoleIdToUsersTable1683039918811 } from './migrations/1683039918811-AddRoleIdToUsersTable'
+import { User } from '@users/entities/User'
 
 export const dataSource = new DataSource({
     type: 'sqlite',
     database: './db.sqlite',
-    entities: [Role],
+    entities: [Role, User],
     migrations: [
         CreateRolesTable1666792375084,
         CreateUsersTable1683038820881,
