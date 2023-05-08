d6ed4a000d5969b87bc7d2c841f9ef8f24877feb
matheus damiao <matheus.damiaoliveira@gmail.com>
2023-05-08 09:10:33 -0300

    Add CreateUserController

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -1,8 +1,11 @@
 import { IUsersRepository } from '@users/repositories/IUsersRepository'
 import { UsersRepository } from '@users/repositories/UsersRepository'
+import { CreateUserUseCase } from '@users/useCases/createUser/CreateUserUseCase'
 import { container } from 'tsyringe'
 
 container.registerSingleton<IUsersRepository>(
     'UsersRepository',
     UsersRepository,
 )
+
+container.registerSingleton('CreateUserUseCase', CreateUserUseCase)
