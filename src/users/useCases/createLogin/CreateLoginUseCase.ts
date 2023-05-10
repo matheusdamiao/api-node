3cfa28ad515b697bd028f024b9050cde8e6213ad
matheus damiao <matheus.damiaoliveira@gmail.com>
2023-05-10 14:20:21 -0300

    Create use case Create Login and add JWT config

---
+++ src/users/useCases/createLogin/CreateLoginUseCase.ts
@@ -0,0 +1,47 @@
+import JwtConfig from '@config/auth'
+import { AppError } from '@shared/errors/AppError'
+import { User } from '@users/entities/User'
+import { IUsersRepository } from '@users/repositories/IUsersRepository'
+import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+import { inject, injectable } from 'tsyringe'
+
+type CreateLoginDTO = {
+    email: string
+    password: string
+}
+
+type IResponse = {
+    user: User
+    token: string
+}
+
+@injectable()
+export class CreateUserUseCase {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+    ) {}
+
+    async execute({ email, password }: CreateLoginDTO): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email)
+        if (!user) {
+            throw new AppError('User email/password combination incorrect')
+        }
+
+        const passwordChecked = await compare(password, user.password)
+        if (!passwordChecked) {
+            throw new AppError('User email/password combination incorrect')
+        }
+
+        const token = sign({}, JwtConfig.jwt.secret, {
+            subject: user.id,
+            expiresIn: JwtConfig.jwt.expiresIn,
+        })
+
+        return {
+            user,
+            token,
+        }
+    }
+}
