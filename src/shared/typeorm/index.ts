af08f63dd6b2edabd42ed54462bfd6b188d0d5e2
matheus damiao <matheus.damiaoliveira@gmail.com>
2023-05-16 11:35:38 -0300

    Create Refresh token entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -5,11 +5,12 @@ import { CreateUsersTable1683038820881 } from './migrations/1683038820881-Create
 import { AddRoleIdToUsersTable1683039918811 } from './migrations/1683039918811-AddRoleIdToUsersTable'
 import { User } from '@users/entities/User'
 import { CreateRefreshTokensTable1684184365822 } from './migrations/1684184365822-CreateRefreshTokensTable'
+import { RefreshToken } from '@users/entities/RefreshToken'
 
 export const dataSource = new DataSource({
     type: 'sqlite',
     database: './db.sqlite',
-    entities: [Role, User],
+    entities: [Role, User, RefreshToken],
     migrations: [
         CreateRolesTable1666792375084,
         CreateUsersTable1683038820881,
