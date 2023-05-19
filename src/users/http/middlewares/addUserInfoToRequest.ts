534c195a9e12a43200fc48a40ffbfbf71a1ef45d
matheus damiao <matheus.damiaoliveira@gmail.com>
2023-05-19 18:59:13 -0300

    Create addUserInfoToRequests middleware to refreshToken

---
+++ src/users/http/middlewares/addUserInfoToRequest.ts
@@ -0,0 +1,44 @@
+import { AppError } from '@shared/errors/AppError'
+import { NextFunction, Request, Response } from 'express'
+import { Secret, decode, verify } from 'jsonwebtoken'
+import authConfig from '@config/auth'
+
+type JwtPayloadProps = {
+    sub: string
+}
+
+export const addUserInfoToRequests = (
+    request: Request,
+    response: Response,
+    next: NextFunction,
+) => {
+    const authHeader = request.headers.authorization
+    if (!authHeader) {
+        return response.status(401).json({
+            error: true,
+            code: 'token.invalid',
+            message: 'Access token not present',
+        })
+    }
+
+    const token = authHeader.replace('Bearer ', '')
+    if (!token) {
+        return response.status(401).json({
+            error: true,
+            code: 'token.invalid',
+            message: 'Access token not present',
+        })
+    }
+    try {
+        const decodedToken = decode(token)
+        const { sub } = decodedToken as JwtPayloadProps
+        request.user = { id: sub }
+        return next()
+    } catch (error) {
+        return response.status(401).json({
+            error: true,
+            code: 'token.invalid',
+            message: 'Access token not present',
+        })
+    }
+}
